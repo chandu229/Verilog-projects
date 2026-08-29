898d6fc20fc3cf86043ef326f85b1d845e26548c
chandu229 <koppalagopichandu@gmail.com>
2026-08-29 07:50:30 +0530

    Enhance testbench with waveform dumping
    Added waveform dumping for simulation analysis.

--- converters/grey to binary/greytobinary_tb.v
+++ converters/grey to binary/greytobinary_tb.v
@@ -1,3 +1,4 @@
+
 `timescale 1ns/1ps
 module greytobinary_tb;
 wire B1,B2,B3,B4;
@@ -8,7 +9,8 @@ initial begin
 
  $display("G1 G2 G3 G4 | B1 B2 B3 B4 ");
 $monitor ("%b %b %b %b | %b %b %b %b",G1,G2,G3,G4,B1,B2,B3,B4);
-	
+  $dumpfile("gtb.vcd");
+  $dumpvars(0, greytobinary_tb);
 #1 G1=0; G2=0; G3=0;G4=0;
 #1 G1=0; G2=0; G3=0;G4=1;
 #1 G1=0; G2=0; G3=1;G4=0;
