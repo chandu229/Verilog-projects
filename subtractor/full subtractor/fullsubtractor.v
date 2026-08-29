db0783c8f9df01820a360bea6c9ab40404e73022
chandu229 <koppalagopichandu@gmail.com>
2026-08-29 15:27:31 +0530

    Delete subtractor directory

--- subtractor/full subtractor/fullsubtractor.v
+++
@@ -1,6 +0,0 @@
-module fullsubtractor(A,B,Bin,D,Bout);
-  input A,B,Bin;
-  output D,Bout;
-  assign D = A^B^Bin;
-  assign Bout = (~A & B) | (~A & Bin) | (B & Bin);
-endmodule
--- subtractor/half subtractor/halfsubtractor.v
+++
@@ -1,7 +0,0 @@
-module halfsubtractor(A,B,D,Bout);
-  input A,B;
-  output D,Bout;
-  assign D = A^B;
-  assign Bout = ~A&B;
-endmodule
-  
